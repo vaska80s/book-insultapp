ed0edfd5b59d8296bc7553df5ad3cdeed6f0b888
Серов Василий Юрьевич <Serov.V.Y@sberbank.ru>
2020-02-16 12:56:51 +0300

    DB test

--- src/main/java/org/openshift/InsultGenerator.java
+++ src/main/java/org/openshift/InsultGenerator.java
@@ -16,8 +16,12 @@ public class InsultGenerator {
             Connection connection = DriverManager.getConnection(databaseURL, username, password);
 
             if(connection != null) {
+//                String SQL = "select a.string AS first, b.string AS second, c.string AS noun " +
+//                        "from short_adjective a , long_adjective b, noun c ORDER BY random() limit 1";
+
                 String SQL = "select a.string AS first, b.string AS second, c.string AS noun " +
-                        "from short_adjective a , long_adjective b, noun c ORDER BY random() limit 1";
+                        "from short_adjective a , long_adjective b, noun c ORDER BY a.id, b.id, c.id limit 1";
+
                 Statement stmt = connection.createStatement();
                 ResultSet rs = stmt.executeQuery(SQL);
                 while(rs.next()) {
